5afe9ce5a16521998378068a3600ef3343b1c02c
root <kevohm>
2022-05-31 15:38:47 +0000

    fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 for i in range(1, 90):
+    check = True
     q = i % 10
     m = i / 10
     m += (10 * q)
     for j in range(1, 10):
         j += (10 * j)
-        print(j)
         if (i == j):
-            pass
-    if (m >= i):
+            check = False
+    if (m >= i and check):
         print("{}".format(i), end=", ")
