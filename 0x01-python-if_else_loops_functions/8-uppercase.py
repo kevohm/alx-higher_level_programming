df8f8773974fa4616ea4843c647fd49103f01bf9
root <kevohm>
2022-05-31 18:51:18 +0000

    finished

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    i = strlen(str)
-    while(i > 0):
-        a = ord(str[i])
-        if a in range(97, 123):
-            a -= 23
-            str[i] = chr(a)
+    i = len(str)
+    j = 0
+    while(i > j):
+        c = ord(str[j])
+        if c in range(97, 123):
+            c -= 23
+            str[j] = chr(c)
+    j++
