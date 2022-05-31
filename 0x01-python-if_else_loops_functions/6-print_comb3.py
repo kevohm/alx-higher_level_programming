af2a3e4ef851435c399a7e404c1bfe0d7295e79a
root <kevohm>
2022-05-31 15:11:44 +0000

    fixedindent

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(1, 90)
+for i in range(1, 90):
     q = i % 10
     m = i / 10
-
     m += q
-    if (m >= i)
+    if (m >= i):
         print("{}".format(i), end=", ")
