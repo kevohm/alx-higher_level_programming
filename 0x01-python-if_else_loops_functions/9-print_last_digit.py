7ed1de9a7b85a56c0be16ed028023b95c3f1eb56
root <kevohm>
2022-06-01 06:32:07 +0000

    finished

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if(type(number) != int or float):
-        print("Traceback (most recent call last):")
-        pass
     a = abs(number) % 10
     return (a)
