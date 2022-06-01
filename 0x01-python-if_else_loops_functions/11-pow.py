ba9987d180a2c7f586ff164c814ee18813b0972b
root <kevohm>
2022-06-01 07:04:35 +0000

    done

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 def pow(a, b):
+    check = False
     if(b < 0):
         b = abs(b)
         a = 1 / a
+        check = True
     if(b == 0):
         return(1)
     b -= 1
-    return round((a *  pow(a, b)), 2)
+    if(check):
+        return round((a *  pow(a, b)), 2)
+    return (a * pow(a, b))
