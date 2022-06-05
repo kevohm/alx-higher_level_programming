12b9aa9b80d0e0e1ba39d9cbfa3851a6be8bceea
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-06-05 19:03:58 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     def element_at(my_list, idx):
         length = len(my_list) 
-        if idx < 0 or idx > length:
+        if (idx < 0) or (idx > length):
             return None
         else:
             print("Element at index {} is {}".format(idx, my_list[idx]))
