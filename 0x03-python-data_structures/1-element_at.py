e84234353cdf95369b6d8a1aa3bd17cc78a368cf
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-06-05 17:49:44 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print("hello Phyllis")
+if __name__ == "__main__":
+    def element_at(my_list, idx):
+        length = len(my_list) 
+        if idx < 0 or idx > length:
+            return None
+        else:
+            print("")
