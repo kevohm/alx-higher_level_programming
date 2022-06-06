7125f73816cd9d6785d1d028f5bcd6845026a8b2
kevohm <kevinkbet@gmail.com>
2022-06-06 05:00:01 -0700

    fixed indent

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        length = len(my_list) 
-        if (idx < 0) or (idx > length - 1):
-            return None
-        else:
-            print("Element at index {} is {}".format(idx, my_list[idx]))
+def element_at(my_list, idx):
+    length = len(my_list)
+    if (idx < 0) or (idx > length - 1):
+        return None
+    else:
+        print("Element at index {} is {}".format(idx, my_list[idx]))
