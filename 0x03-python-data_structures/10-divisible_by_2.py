8454699e0cdbee315fd61864f0e5fb3e20be7314
kevohm <kevinkbet@gmail.com>
2022-06-06 08:41:40 -0700

    done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@
 def divisible_by_2(my_list=[]):
     cpy = my_list
     for i in range(len(my_list)):
-        if(i%2 == 0):
+        if((my_list[i] % 2) == 0):
             cpy[i] = True
         else:
             cpy[i] = False
