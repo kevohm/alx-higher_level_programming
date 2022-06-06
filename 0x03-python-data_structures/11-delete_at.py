f94acd7e619ba24d19cadbcdf65a1bb7e7e59509
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-06-06 19:20:32 +0300

    Update 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+def delete_at(my_list=[], idx=0):
+    cpy = []
+    if (len(my_list) != 0) and (idx > 0) and (idx < len(my_list)):
+        for i in range(len(my_list)):
+            if i == idx:
+                continue
+            cpy.append(my_list[i])
+    else:
+        return my_list
+    return cpy
