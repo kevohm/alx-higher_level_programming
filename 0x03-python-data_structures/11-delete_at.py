aed705d46ae782ff8d561a4e6e03476dde38ed03
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-06-06 19:30:37 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+# 11-delete_at.py
+
+
 def delete_at(my_list=[], idx=0):
-    cpy = []
-    if (len(my_list) != 0) and (idx > 0) and (idx < len(my_list)):
-        for i in range(len(my_list)):
-            if i == idx:
-                continue
-            cpy.append(my_list[i])
-    else:
-        return my_list
-    return cpy
+    """Delete an item at a specified pos in list."""
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
