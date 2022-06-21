87c5843d26f978936abc54c801809006c41f4e5d
kevohm <kevinkbet@gmail.com>
2022-06-21 06:53:16 -0700

    done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -36,3 +36,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def size(self):
+        """ getter """
+        return self.__size
