9df201bed2aab01d52a57a3104a627fd81f03dfa
kevohm <kevinkbet@gmail.com>
2022-06-21 05:31:50 -0700

    done

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -13,14 +13,7 @@ class Square:
             size (int): size
         """
         if size != int(size):
-<<<<<<< HEAD
             raise TypeError("Not a int")
         if size < 0:
             raise ValueError("Value is invalid")
         self.__size = size
-=======
-            raise TypeError("size must be of integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.size = size
->>>>>>> ddcedebc85d64c4c83d0f6bb549a0b5506ad7ff8
