053d8f5feb6d8df46e3ecd70045c5ca0b1353e85
kevohm <kevinkbet@gmail.com>
2022-06-21 05:51:51 -0700

    done

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 2-square.py
+
+"""square class"""
+
+class Square:
+    """inside class"""
+    def __init__(self, size=0):
+        """
+        Args:
+            size (int): integer
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  
