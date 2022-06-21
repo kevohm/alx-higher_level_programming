b195bb1cbf72666cfeee3ff4358796bd5f2b6bd0
kevohm <kevinkbet@gmail.com>
2022-06-21 06:02:08 -0700

    done

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 3-square.py
+
+"""square class"""
+
+class Square:
+    """ inside class"""
+    def __init__(self, size=0):
+        """
+        Args:
+            size (int): integer
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return pow(self.__size, 2)
