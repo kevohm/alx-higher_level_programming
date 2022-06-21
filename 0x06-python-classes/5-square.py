ce958b2c27a800cd627f3d5d894c5fe76053a0ed
kevohm <kevinkbet@gmail.com>
2022-06-21 07:18:16 -0700

    done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -40,7 +40,9 @@ class Square:
 
     def my_print(self):
         """ print to stdout """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
         if self.__size == 0:
             print("")
-        else:
-            print("#")
