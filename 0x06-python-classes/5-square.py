fad23dfd0244c9831f2013b17fd503eccb25675c
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-06-21 17:09:45 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,2 +1,39 @@
 #!/usr/bin/python3
 
+
+# 5-square.py
+
+
+""" class of square """
+
+
+class Square:
+
+    """ inside square """
+    def __init__(self, size=0):
+        """
+        Args:
+            size (int): integer
+        """
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Get and set """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if value != int(value):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ area of square """
+        return pow(self.__size, 2)
