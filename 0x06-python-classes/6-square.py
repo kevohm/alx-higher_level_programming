73964bf9b4a05bbeef073694908270245a4910a8
kevohm <kevinkbet@gmail.com>
2022-06-21 07:37:58 -0700

    done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 
 
-# 5-square.py
+# 6-square.py
 
 
 """ class of square """
@@ -11,16 +10,18 @@
 class Square:
 
     """ inside square """
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Args:
             size (int): integer
+            position (int, int): tuple
         """
         if size != int(size):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -35,12 +36,29 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """ get and set """
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        """
+        Args:
+            value (tuple): tuple
+        """
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
         """ area of square """
         return pow(self.__size, 2)
 
     def my_print(self):
         """ print to stdout """
+        for x in range(0, self._position[1]):
+            print("")
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
