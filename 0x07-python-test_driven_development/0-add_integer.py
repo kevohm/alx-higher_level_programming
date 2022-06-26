7e3e2b3af4365f326e8bf0691a23f816aebcf1ee
kevohm <kevinkbet@gmail.com>
2022-06-26 02:11:18 -0700

    done

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 # 0-add_integer.py
 
-""" add_integer module """
+"""add_integer module
+
+This module add two integers and rounds float
+
+"""
 
 
 def add_integer(a, b=98):
+    ''' add_integer function
+    Return: result of  a plus b
     '''
-    Args:
-        a (int): integer
-        b (int): integer
-    Returns:
-        int: a plus b
-    '''
-    if not isinstance(a,(int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(round(a)) + int(round(b))
