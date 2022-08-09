60b6d1c9ab9edf1c1445096ae99bc1f2f37bca2e
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-08-09 12:01:16 +0300

    Update 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- INSERT first record
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, "John", 10);
+-- INSERT second record
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+-- INSERT third record
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+-- INSERT fourth record
+INSERT INTO second_table (`id`, `name`, `score`) VALUES (4, "George", 8);
