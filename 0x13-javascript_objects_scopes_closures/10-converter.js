cba4b327cc45365f47a7485bc7310708a99e25b2
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-09-06 23:44:54 +0300

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const converter = function (base) {
-  return function (num) { return parseInt(num + '', 10).toString(base); };
+  return function (val) {
+    return parseInt(val + '', 10).toString(base);
+  };
 };
 module.exports = { converter };
