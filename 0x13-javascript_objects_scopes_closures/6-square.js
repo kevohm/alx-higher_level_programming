845b6d453e9a1d5623b088dddb81d8822ed33378
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-09-06 23:51:28 +0300

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,18 +1,19 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    const sym = (c) || 'x';
+    const char = c ?? 'X';
+    let str;
     for (let i = 0; i < this.height; i++) {
+      str = '';
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write(sym);
+        str += char;
       }
-      process.stdout.write('\n');
+      console.log(str);
     }
   }
-}
-module.exports = Square;
+};
