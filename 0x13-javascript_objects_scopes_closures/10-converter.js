eb8e910d9ebf8bae606cb7dfca30a6a658d0a3bd
kevohm <kevinkbet@gmail.com>
2022-09-06 13:35:10 -0700

    finished

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const converter = function (base) {
-  return function (num) { return parseInt(num, base); };
+  return function (num) { return parseInt(num + "", 10).toString(base); };
 };
 module.exports = { converter };
