ec4e361ba094f8f438fb54ed92a49fa00f824f29
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-09-22 23:58:48 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-
+# To sends a POST request to the passed URL, and displays the body of the response A variable email must be sent with the value test@gmail.com A variable subject must be sent with the value I will always be here for PLD
+curl -s -X "POST" -d "email=test@gmail.com&subject=I will always be there for PLD" "$1"
