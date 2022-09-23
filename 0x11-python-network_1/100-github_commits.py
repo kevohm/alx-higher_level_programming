af8e3874f42d3f647be0a64210bf0c078abbbbd6
kevohm <kevinkbet@gmail.com>
2022-09-23 12:48:18 -0700

    finished_task_100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -3,10 +3,10 @@
 from sys import argv
 import requests
 if len(argv) == 3:
-    url='https://api.github.com/repos/{}/{}/commits?per_page=10'
+    url = 'https://api.github.com/repos/{}/{}/commits?per_page=10'
     data = requests.get(url.format(argv[2], argv[1]))
     r_dict = data.json()
     for i in r_dict:
         sha = i.get('sha')
         author = i.get('commit').get('author').get('name')
-        print(f'{sha}: {author}') 
+        print(f'{sha}: {author}')
