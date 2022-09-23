c2783a5267788123faa0641e41d9a05269e074c0
kevohm <kevinkbet@gmail.com>
2022-09-23 04:32:42 -0700

    finished

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status"""
+import requests
 
+data = requests.get("https://alx-intranet.hbtn.io/status")
+print("Body response:")
+print("\t- type: {}".format(type(data.text)))
+print("\t- content: {}".format(data.text))
