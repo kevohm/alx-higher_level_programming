625daab22c9053f08b1d66cb0d9840dcc42a13d6
kevohm <kevinkbet@gmail.com>
2022-09-23 05:27:09 -0700

    finished_task_7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" displays the body of the response"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    try:
+        data = requests.get(sys.argv[1])
+        if data.status_code > 400:
+            data.raise_for_status()
+        print(data.text)
+    except requests.exceptions.HTTPError as e:
+        print(e)
