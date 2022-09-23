1774956fe6907b448914d781c046b702984f6278
kevohm <kevinkbet@gmail.com>
2022-09-23 04:17:33 -0700

    finished_task_3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""displays the body of the response (decoded in utf-8)"""
+from urllib.error import HTTPError
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        try:
+            with request.urlopen(sys.argv[1]) as r:
+                print(r.read().decode("utf-8"))
+        except HTTPError as e:
+            print(e)
