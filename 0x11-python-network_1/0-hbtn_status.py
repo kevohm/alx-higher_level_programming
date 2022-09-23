8bf75a0dac56c1501d45ab11e13247c49ea39459
kevohm <kevinkbet@gmail.com>
2022-09-23 03:08:26 -0700

    changed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ from urllib import request
 
 url = 'https://alx-intranet.hbtn.io/status'
 with request.urlopen(url) as response:
+    print("Body response:")
     page = response.read()
     t_data = type(page)
     decoded = page.decode("UTF-8")
-    data = 'Body response: \n\t - type:
-        {}\n\t - content: {}\n\t - utf8 content: {}'
+    data = '\t - type:{}\n\t - content: {}\n\t - utf8 content: {}'
     print(data.format(page, t_data, decoded))
