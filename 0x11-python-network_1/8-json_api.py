0916e87dc9af8e60c9af7ad79f636a9181868226
kevohm <kevinkbet@gmail.com>
2022-09-23 06:24:10 -0700

    fixed_data

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,11 +2,12 @@
 """checks if response json"""
 import requests
 import sys
+
 p = ""
 if len(sys.argv) == 2:
     p = sys.argv[1]
 else:
-    data = requests.post("http://0.0.0.0:5000/search_user", params={"q": p})
+    data = requests.post("http://0.0.0.0:5000/search_user", data={"q": p})
     try:
         j_d = data.json()
         if len(J_d.keys()) == 0:
