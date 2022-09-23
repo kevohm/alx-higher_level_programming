cefb9581997d11db05441cd7184bb567042ebc62
kevohm <kevinkbet@gmail.com>
2022-09-23 05:28:05 -0700

    finished_it

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
             data.raise_for_status()
         print(data.text)
     except requests.exceptions.HTTPError as e:
-        print(e)
+        print("Error code: {}".format(e.code))
