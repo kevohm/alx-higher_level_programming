9fa1bc3e84c95c336bb43343e0772f6f6a70e1b7
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2022-09-23 16:44:06 +0300

    Update 9-starwars.py

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 """Python script that takes your GitHub credentials and uses the GitHub API to display your id"""
+import requests
+from sys import argv
 
+if __name__ == "__main__":
+    urlSearch = "https://swapi.co/api/people/?search={}".format(argv[1])
+    response = requests.get(urlSearch)
+    responseDict = response.json()
+    resCount = responseDict.get('count')
+    resResults = responseDict.get('results')
+    print("Number of results: {}".format(resCount))
+    for d in resResults:
+        print("{}".format(d.get("name")))
