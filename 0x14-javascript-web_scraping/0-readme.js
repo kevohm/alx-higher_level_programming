e460ffe208ed262b4f89d1f0b757d7c1fefb1e96
kevin kibet <kevinkipkibet@gmail.com>
2022-10-18 22:25:09 +0300

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
 #!/bin/node
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 const content = readFileSync(process.argv[2], 'utf-8');
-console.log(content)
+console.log(content);
